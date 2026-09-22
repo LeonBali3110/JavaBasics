df3f905534281d1129063988d765a12c78a671d6
Leon <leon@hechosheroicos.com>
2026-09-22 10:25:31 +0200

    added a new file for non scored programs

---
+++ 00_ExercicisClasse/NomEdat.java
@@ -0,0 +1,10 @@
+public class NomEdat {
+	public static void main(String args[]){
+		String nom = "León";
+		String cognom = "Bali";
+		String nomcompleto = nom + " " + cognom;
+		int edat = 24;
+		System.out.println("Em dic " + nomcompleto + " i tinc " + edat + " anys");
+		
+	}
+}
