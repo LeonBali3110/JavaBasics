de7b970ae65f58e2c0216b320043b66196f26b1c
Leon <leon@hechosheroicos.com>
2026-09-22 12:27:00 +0200

    new programs added. Practicing variables and operators

---
+++ 01_02-CubicEquation/CubicEquation++.java
@@ -0,0 +1,17 @@
+public class CubicEquation++ {
+
+	public static void main(String[] args) {
+		int x = 3;
+		double f = (Math.pow(x, 3)) + 2 * (Math.pow(x, 2)) + (4 * x) + 5;
+		System.out.println("f(" + x + ")" + " = " + f);	
+
+        x = 5;
+		f = (Math.pow(x, 3)) + 2 * (Math.pow(x, 2)) + (4 * x) + 5;
+		System.out.println("f(" + x + ")" + " = " + f);	
+		
+		x = 10;
+		f = (Math.pow(x, 3)) + 2 * (Math.pow(x, 2)) + (4 * x) + 5;
+		System.out.println("f(" + x + ")" + " = " + f);	
+
+	}
+}
---
+++ 01_03-PowerFormula/PowerFormula.java
@@ -0,0 +1,11 @@
+public class PowerFormula {
+
+	public static void main(String[] args) {
+		int force = 125;
+		double distance = 37;
+		int time = 12;
+	
+		double power = force * distance / time;
+		System.out.println("Power = " + power + " W");		 
+	}
+}
