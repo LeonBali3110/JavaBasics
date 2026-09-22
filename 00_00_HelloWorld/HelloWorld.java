ef59d6458f2c5d20a50b6bbf12cee3025b858d4f
Leon <leon@hechosheroicos.com>
2026-09-22 10:54:49 +0200

    Add the exercise 01_01

--- 00_00_HelloWorld/HelloWorld.java
+++ 00_00_HelloWorld/HelloWorld.java
@@ -1,7 +1,14 @@
 public class HelloWorld {
-	
+	//imprimer per pantalla Hello World!
+	/* Aquest 
+	es un 
+	comentari 
+	llarg
+	*/
 	public static void main(String args[]){
-		System.out.println("Hello, World!");
+		int edat = 2500;
+		edat = edat * 2;
+		System.out.println("Hello, World! " + edat);
 		
 	}
 }
